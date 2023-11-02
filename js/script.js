89a54d20b92333e819a81519a6bf838f1db75869
melisaagstn <melisaagstn29@gmail.com>
2023-11-02 15:38:34 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+
+    /*==================== sticky navbar ====================*/
+
+    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+/*==================== scroll reveal ====================*/
+
+
+/*==================== typed js ====================*/
